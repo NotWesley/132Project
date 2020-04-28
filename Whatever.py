73aa9a60bd677646508183442b1415cebecddafe
Wesley Goins <wesleymgoins@gmail.com>
2020-04-28 17:22:47 -0500

    new beginning

--- Whatever.py
+++ Whatever.py
@@ -1,16 +1,1 @@
-def function1():
-    print "Yo"
-
-def function2():
-    print "Anky" * 9
-
-def function3():
-    pass
-
-def functionWhatever():
-    pass
-
-function1()
-function2()
-function3()
-functionWhatever()
+import pygame
